35ba50b3d0fa405acca643d11c479fe957adeb0b
jbxcruz <johnbertcruz2020@gmail.com>
2024-11-26 20:16:17 +0800

    Update script.js

--- src/script.js
+++ src/script.js
@@ -68,20 +68,20 @@ floor.rotation.x = - Math.PI * 0.5 // Rotate to lay flat
 floor.position.y = -1 // Position on the ground level
 scene.add(floor)
 
-/**
- * Lights
- */
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+// Lights
+const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.12)  // Dim and blue-ish light
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
-const moonLight = new THREE.DirectionalLight('#ffffff', 0.5)
+const moonLight = new THREE.DirectionalLight('#b9d5ff', 0.12)  // Dim and blue-ish light
 moonLight.position.set(4, 5, -2)
 gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(moonLight.position, 'x').min(-5).max(5).step(0.001)
 gui.add(moonLight.position, 'y').min(-5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(-5).max(5).step(0.001)
 scene.add(moonLight)
+
+
 
 /**
  * Sizes
@@ -101,26 +101,6 @@ window.addEventListener('resize', () => {
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
-
-
-
-
-
-
-// Lights
-const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.12)  // Dim and blue-ish light
-gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
-scene.add(ambientLight)
-
-const moonLight = new THREE.DirectionalLight('#b9d5ff', 0.12)  // Dim and blue-ish light
-moonLight.position.set(4, 5, -2)
-gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
-gui.add(moonLight.position, 'x').min(-5).max(5).step(0.001)
-gui.add(moonLight.position, 'y').min(-5).max(5).step(0.001)
-gui.add(moonLight.position, 'z').min(-5).max(5).step(0.001)
-scene.add(moonLight)
-
-
 
 
 
