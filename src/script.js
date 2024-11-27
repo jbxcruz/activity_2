51269b1847cbae833182fabbde94f9fdd737d4dd
jbxcruz <johnbertcruz2020@gmail.com>
2024-11-27 12:29:50 +0800

    Update script.js

--- src/script.js
+++ src/script.js
@@ -29,14 +29,6 @@ scene.fog = fog
  */
 const textureLoader = new THREE.TextureLoader()
 
-// Load door textures
-const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
-const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
-const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
-const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
-const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
-const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
 // Load wall textures
 const bricksColorTexture = textureLoader.load('/textures/bricks/color.jpg')
